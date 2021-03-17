f7ae054c51b32c64a3ec109a00310ae7ef71ba71
Sebastian Ruiz <sebastianruizalvarez@gmail.com>
2021-03-16 22:47:31 -0600

    Se estudió la forma de utilizar las promesas en JS

---
+++ promise/index.js
@@ -0,0 +1,40 @@
+const somethingWillHappen=()=>{
+    return new Promise((resolve,reject)=>{
+        if (true){
+            resolve('Hey! Lo logramos');
+        }else{
+            reject('Upps ni pepe krnal');
+        }
+    })
+}
+
+
+
+// somethingWillHappen()
+//     .then(response=>console.log(response))
+//     .catch(err=>console.error(err));
+
+
+
+
+const somethingWillHappen2=()=>{
+    return new Promise((resolve,reject)=>{
+        if(true){
+            setTimeout(()=>{
+                resolve('True');
+            },2000)
+        }else{
+            const error=new Error('Ups');
+            reject(error);
+        }
+    })
+}
+
+// somethingWillHappen2()
+//     .then(response=>console.log(response))
+//     .catch(err=>console.error(err));
+
+
+Promise.all([somethingWillHappen(),somethingWillHappen2])
+    .then(response=>console.log('Array of results',response))
+    .catch(err=>console.error(err));
