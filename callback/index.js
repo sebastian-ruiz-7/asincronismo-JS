2ebec7086c5ec7d771334062bf638db0d08096ac
Sebastian Ruiz <sebastianruizalvarez@gmail.com>
2021-03-16 21:22:33 -0600

    Se estudió la estructura básica de un callback

---
+++ callback/index.js
@@ -0,0 +1,25 @@
+const sum=(num1,num2)=>{
+    return num1+num2;
+}
+
+const calc=(num1,num2,callback)=>{
+    return callback(num1,num2);
+}
+
+console.log(calc(6,4,sum));
+
+
+
+const date=callback=>{
+    console.log(new Date);
+    setTimeout(()=>{
+        let date=new Date;
+        callback(date);
+    },3000)
+}
+
+const printDate=(dateNow)=>{
+    console.log(dateNow);
+}
+
+date(printDate);
