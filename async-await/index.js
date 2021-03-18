c68306f59fd902f23ff5b13346d25fd3531f372f
Sebastian Ruiz <sebastianruizalvarez@gmail.com>
2021-03-17 21:39:45 -0600

    Se estudió el uso del asyc/await como funciones asincronas

---
+++ async-await/index.js
@@ -0,0 +1,25 @@
+const doSomethingAsyn=()=>{
+    return new Promise((resolve,reject)=>{
+        (true)
+            ? setTimeout(() => {resolve('Do something Async')}, 2000)
+            :reject(new Error('Test Error'))
+    })
+}
+
+const doSomething=async()=>{
+    const something=await doSomethingAsyn()
+    console.log(something);
+}
+
+
+const anotherFunction= async()=>{
+    try{
+    const something=await doSomethingAsyn()
+    }catch{
+        console.error(error)
+    }
+}
+
+console.log('Before');
+anotherFunction();
+console.log('After');
